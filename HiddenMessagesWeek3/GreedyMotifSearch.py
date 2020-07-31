c14ad82bd6e46b53dd9d418144676223f8052afd
GinaK <avalanchec@hotmail.com>
2020-07-31 03:05:52 +0200

    ...not sure where is wrong

--- HiddenMessagesWeek3/GreedyMotifSearch.py
+++ HiddenMessagesWeek3/GreedyMotifSearch.py
@@ -1,2 +1,14 @@
-def GreedyMotifSearch(Dna, k, t):
+def GreedyMotifSearch(Dna, k, t):  
+    Dna = Dna.split('\n')
+    Dna1 = Dna[0]
+    BestMotifs = []
+    for row in range(t):
+        Dnarow = Dna[row]
+        Motif = Dnarow[0:k-1]
+        BestMotifs.append(Motif)
     
+    for i in range(len(Dna1)- k + 1):
+        iter_motif = Dna1[i:i+k-1]
+        for j in [1:t]:
+            
+
